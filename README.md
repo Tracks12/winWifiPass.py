6da26620788bccc23ef63c2db6976f897a816ae6
Tracks12 <cardinal.florian.erwan@gmail.com>
2020-11-24 12:40:35 +0100

    add files to repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# winWifiPass.py
+
+Get windows wifi password registered
+
+## Licence
+
+Code sous license [GPL v3](LICENSE)
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+from subprocess import check_output as shell
+
+def main():
+	return(True)
+
+if(__name__ == "__main__"):
+	main()
